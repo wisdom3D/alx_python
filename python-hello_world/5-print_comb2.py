0ab477effd9c550a59f5577237c10fc68dbbdaa5
wisdom3D <wisdomkoudama@gmail.com>
2023-07-27 23:30:36 +0000

    new commit

--- python-hello_world/5-print_comb2.py
+++ python-hello_world/5-print_comb2.py
@@ -1,2 +1,5 @@
 for i in range(100):
-        print(f"{i:02d}", end=", " if i < 99 else "\n")
+        if i < 99:
+                print('{:02d}'.format(i), end=",")
+        else:
+                print(i)
