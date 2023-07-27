826d025e1f1c9cab2a9927be1cfc9a629a8d7413
wisdom3D <wisdomkoudama@gmail.com>
2023-07-27 22:04:20 +0000

    digit commit

---
+++ python-hello_world/3-last_digit.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+numberL = str(number)[-1]
+numberL = int(numberL)
+if numberL > 5:
+    print('Last digit of',number, 'is', numberL, 'and is greater than 5')
+elif numberL == 0:
+    print('Last digit of',number, 'is', numberL, 'and is 0')
+elif numberL < 6 and numberL != 0:
+    print('Last digit of',number, 'is', numberL, 'and is less than 6 and not 0')
