4b207fc12a7e40868ccbff464a034457b737f6f3
wisdom3D <wisdomkoudama@gmail.com>
2023-07-28 00:33:11 +0000

    New commit

--- python-functions/2-temperature.py
+++ python-functions/2-temperature.py
@@ -1,4 +1,3 @@
 def convert_to_celsius(fahrenheit):
-    celsius = (fahrenheit-32)*(5/9)
+    celsius = (fahrenheit-32)*0.555555556
     return print("{:.2f}".format(celsius))
-convert_to_celsius(-459.67)
