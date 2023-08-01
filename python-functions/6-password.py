316884d12ff5a3f21f6446d8c78cd613652ca5a5
wisdom3D <wisdomkoudama@gmail.com>
2023-08-01 14:45:26 +0000

    new commit

---
+++ python-functions/6-password.py
@@ -0,0 +1,12 @@
+def validate_password(password):
+    if len(password) < 8:
+        return False
+    contain_uppercas = any(char.isupper() for char in password)
+    contain_lowercase = any(char.islower() for char in password)
+    contain_digit = any(char.isdigit() for char in password)
+    if not (contain_uppercas and contain_lowercase and contain_digit):
+        return False
+    if ' ' in password:
+        return False
+    return True
+print(validate_password("abc123"))
